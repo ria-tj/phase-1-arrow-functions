4cab2bc870f8ce55eba2ab74fe560a7da0943e90
Ria <johnria6@gmail.com>
2024-07-02 16:52:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(divide) {
+    return 2000 / 100;
+    console.log(divide(2000/100));
+}
+const square = (num) => {
+    return num * num
+};
+console.log(square(2));
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(4,3); 
